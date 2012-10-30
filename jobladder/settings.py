b3dc2d60f935e6bd512e7e715080a8c10197f7d1
Nicolas Diaz Aragon <nico.diaz.aragon@gmail.com>
2012-10-30 12:04:53 -0500

    ok

--- jobladder/settings.py
+++ jobladder/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
     'gunicorn',
 )
 
