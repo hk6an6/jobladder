c304b8201676ee4f32ac18dc7f73276756c768bf
Nicolas Diaz Aragon <nico.diaz.aragon@gmail.com>
2012-10-31 15:41:43 -0500

    fixed admin static files

--- jobladder/settings.py
+++ jobladder/settings.py
@@ -2,8 +2,9 @@
 import dj_database_url
 import os
 
-PROJECT_DIR = os.path.dirname(__file__) #this is not a django setting
-DEBUG = True
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = PROJECT_ROOT
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -59,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+STATIC_ROOT = ''#os.path.join(PROJECT_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
