5ac67e594f22ce0ab26ed8c3bb5d448f7f5584dc
Nicolas Diaz Aragon <nico.diaz.aragon@gmail.com>
2012-11-02 12:21:01 -0500

    fixing bash profiles

--- fix_local_keys.sh
+++ fix_local_keys.sh
@@ -3,5 +3,6 @@
 echo "AWS_ACCESS_KEY_ID='AWS_ACCESS_KEY_ID'" >> ~/.bash_profile
 echo "AWS_SECRET_ACCESS_KEY='AWS_SECRET_ACCESS_KEY'" >> ~/.bash_profile
 echo "AWS_STORAGE_BUCKET_NAME='AWS_STORAGE_BUCKET_NAME'" >> ~/.bash_profile
+echo "https://serempre.repositoryhosting.com/git/serempre/jobladder.git" >> ~./bash_profile
 
 
