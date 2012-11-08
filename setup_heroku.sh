82e91cf52128801cc56559661e6a987a7a33a977
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-08 17:39:46 -0500

    added virtualenv setup to heroku install scripts

--- setup_heroku.sh
+++ setup_heroku.sh
@@ -4,3 +4,8 @@
 git remote add heroku git@heroku.com:secret-castle-3861.git
 #this is optional
 #heroku labs:enable user-env-compile -a secret-casstle-3861
+sudo easy_install pip
+sudo pip install virtualenv
+virtualenv venv --distribute
+source venv/bin/activate
+pip install -r requirements.txt
