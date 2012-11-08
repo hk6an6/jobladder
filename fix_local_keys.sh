3d6b1cff7006a7d622fad44299a22813cc6379de
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-08 17:22:05 -0500

    updated scripts for enabling development environments across several workstations

--- fix_local_keys.sh
+++ fix_local_keys.sh
@@ -4,5 +4,17 @@ echo "AWS_ACCESS_KEY_ID='AKIAJ7JRXK43OVKXUM7A'" >> ~/.bash_profile
 echo "AWS_SECRET_ACCESS_KEY='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'" >> ~/.bash_profile
 echo "AWS_STORAGE_BUCKET_NAME='pacific_energy'" >> ~/.bash_profile
 echo "JOBLADDER_GIT=https://serempre.repositoryhosting.com/git/serempre/jobladder.git" >> ~/.bash_profile
-
-
+git config --global user.name 'nicolasaragon'
+git config --global user.email 'nico.diaz.aragon@gmail.com'
+curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+chmod u+x git-credential-osxkeychain
+GIT_PATH=`which git`
+GIT_PATH=$(echo $GIT_PATH | awk '{split($0,A,"/"); for(i=0;i<length(A)-1;i++) { if(A[i]!="") printf "/%s", A[i] } }')
+sudo mv git-credential-osxkeychain $GIT_PATH/
+git config --global credential.helper osxkeychain
+cd ~/.ssh
+mkdir key_backup
+cp id_rsa* key_backup
+rm id_rsa*
+ssh-keygen -t rsa -C "nico.diaz.aragon@gmail.com"
+heroku keys:add ~/.ssh/id_rsa.pub
---
+++ setup_heroku.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#run this within the cloned project folder while being sourced into virtualenv
+
+git remote add heroku git@heroku.com:secret-castle-3861.git
+#this is optional
+#heroku labs:enable user-env-compile -a secret-casstle-3861
