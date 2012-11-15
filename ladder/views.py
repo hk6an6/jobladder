366fab805389029f951cbf9ea621b617e881bfd5
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-15 18:17:30 -0500

    passing all local context variables as a dictionary to the template loader function

--- ladder/views.py
+++ ladder/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def home(request):
-	return render_to_response('ladder/index.html',{})
+	return render_to_response('ladder/index.html',locals())
