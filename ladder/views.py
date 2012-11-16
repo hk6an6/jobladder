ad8e4cd1ec86d1b35dfcb44dc8ae5c86ba5da027
$1 <$2>
2012-11-16 11:24:39 -0500

    using request context to pass request preprocessor information onto templates

--- ladder/views.py
+++ ladder/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
 
 def home(request):
-	return render_to_response('ladder/index.html',locals())
+	return render_to_response('ladder/index.html',locals(),RequestContext(request))
