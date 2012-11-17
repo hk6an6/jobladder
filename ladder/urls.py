284f28d5d1490453ed27e520483900636348236e
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-17 12:47:02 -0500

    restful webservices for querying cargo objects

--- ladder/urls.py
+++ ladder/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^$', 'ladder.views.home'),
+	url(r'^cargo/$','ladder.views.cargo'),
 )
--- ladder/views.py
+++ ladder/views.py
@@ -1,8 +1,13 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core import serializers
+from models import Cargo
 
 # Create your views here.
 
 def home(request):
 	return render_to_response('ladder/index.html',locals(),RequestContext(request))
+
+def cargo(request):
+	return HttpResponse(serializers.serialize('json',Cargo.objects.all()), mimetype='application/json')
