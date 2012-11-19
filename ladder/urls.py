15790f43ebed6f9b7cd114489b9f8ea7b28440a6
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-19 09:35:12 -0500

    using named parameters for url settings

--- ladder/urls.py
+++ ladder/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	url(r'^$', 'ladder.views.home'),
 	url(r'^cargo/$','ladder.views.cargo'),
-	url(r'^cargo/([\w\s]*)/$','ladder.views.cargo'),
+	url(r'^cargo/(?P<cargo_name_fragment>[\w\s]*)/$','ladder.views.cargo'),
 )
--- ladder/views.py
+++ ladder/views.py
@@ -7,16 +7,19 @@ import logging
 
 # Create your views here.
 
+#create a logger that can output content to console. This will be used while in development
 logger = logging.getLogger('console')
 
+#output home page
 def home(request):
 	return render_to_response('ladder/index.html',locals(),RequestContext(request))
 
+#output a json collection holding all known cargos in the database
 def cargo(request,cargo_name_fragment=None):
 	if cargo_name_fragment:
 		logger.debug(cargo_name_fragment)
-		return HttpResponse(serializers.serialize('json',Cargo.objects.filter(nombre__icontains=cargo_name_fragment))[:20], mimetype='application/json')
+		return HttpResponse(serializers.serialize('json',Cargo.objects.filter(nombre__icontains=cargo_name_fragment), fields=('pk','nombre',)), mimetype='application/json')
 	else:
 		logger.debug('output everything')
-		return HttpResponse(serializers.serialize('json',Cargo.objects.all()), mimetype='application/json')
+		return HttpResponse(serializers.serialize('json',Cargo.objects.all(), fields=('pk','nombre',)), mimetype='application/json')
 		
