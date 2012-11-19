fff16209b0de4b6f2aaa225b772c73126168cb3c
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-11-19 14:21:56 -0500

    testing jquery ui autocomplete

--- ladder/urls.py
+++ ladder/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 
+#see https://docs.djangoproject.com/en/dev/topics/http/urls/
+
 urlpatterns = patterns('',
 	url(r'^$', 'ladder.views.home'),
-	url(r'^cargo/$','ladder.views.cargo'),
-	url(r'^cargo/(?P<cargo_name_fragment>[\w\s]*)/$','ladder.views.cargo'),
+	url(r'^cargo/$','ladder.views.cargo', name="get-cargos"),
+	url(r'^cargo/(?P<cargo_name_fragment>[\w\s]*)/$','ladder.views.cargo', name="get-cargos-filter-by-cargo-name"),
 )
