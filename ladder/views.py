fb26d3d242d02604e4050cfdbf7f859bb803e9ff
nicolasaragon <nico.diaz.aragon@gmail.com>
2012-12-02 12:30:58 -0500

    using a slider to pick a head shape for the avatar

--- ladder/views.py
+++ ladder/views.py
@@ -40,6 +40,8 @@ def cargo_by_pk(request, cargo_pk=0):
 	
 def create_avatar(request, origin_pk, target_pk, sex):
 	cuerpos = None
+	caras = CaraAvatar.objects.all()
+	css_len_caras = (len(caras)+1) * 120
 	if(sex == 'H'):
 		cuerpos = CuerpoAvatar.objects.filter(etiqueta__icontains="hombre")
 	else:
