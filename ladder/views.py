04b1a8b9bf3497b4bf714f2e17a1dea1e31396ba
nicolasaragon <nico.diaz.aragon@gmail.com>
2013-01-08 09:49:10 -0500

    loading all assets needed to build an avatar

--- ladder/views.py
+++ ladder/views.py
@@ -41,6 +41,14 @@ def cargo_by_pk(request, cargo_pk=0):
 def create_avatar(request, origin_pk, target_pk, sex):
 	cuerpos = None
 	caras = CaraAvatar.objects.all()
+	faccciones = Faccion.objects.all()
+	pantalones = PantalonAvatar.objects.all()
+	camisas = CamisaAvatar.objects.all()
+	zapatos = ZapatosAvatar.objects.all()
+	accesorios = AccesoriosAvatar.objects.all()
+	cabellos = PeloAvatar.objects.all()
+	cabellos_experiencia = CabelloAvatar.objects.all()
+	sombreros = SombreroAvatar.objects.all()
 	css_len_caras = (len(caras)+1) * 120
 	if(sex == 'H'):
 		cuerpos = CuerpoAvatar.objects.filter(etiqueta__icontains="hombre")
