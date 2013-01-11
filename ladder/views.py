3466b00e2f3159d35ebaf8886969af1b3de468b3
nicolasaragon <nico.diaz.aragon@gmail.com>
2013-01-11 11:29:42 -0500

    added hair to the avatar image

--- ladder/views.py
+++ ladder/views.py
@@ -46,6 +46,7 @@ def create_avatar(request, origin_pk, target_pk, sex):
 	pantalones = None
 	camisas = None
 	zapatos = None
+	pelos = None
 	accesorios = AccesoriosAvatar.objects.all()
 	cabellos = PeloAvatar.objects.all()
 	cabellos_experiencia = CabelloAvatar.objects.all()
@@ -55,14 +56,17 @@ def create_avatar(request, origin_pk, target_pk, sex):
 		pantalones = PantalonAvatar.objects.filter(etiqueta__icontains="hombre")
 		camisas = CamisaAvatar.objects.filter(etiqueta__icontains="hombre")
 		zapatos = ZapatosAvatar.objects.filter(etiqueta__icontains="hombre")
+		pelos = PeloAvatar.objects.filter(etiqueta__icontains="hombre")
 	else:
 		cuerpos = CuerpoAvatar.objects.filter(etiqueta__icontains="mujer")
 		pantalones = PantalonAvatar.objects.filter(etiqueta__icontains="mujer")
 		camisas = CamisaAvatar.objects.filter(etiqueta__icontains="mujer")
 		zapatos = ZapatosAvatar.objects.filter(etiqueta__icontains="mujer")
+		pelos = PeloAvatar.objects.filter(etiqueta__icontains="mujer")
 	cuerpos = cuerpos.order_by('etiqueta','contextura')
 	pantalones = pantalones.order_by('etiqueta','contextura')
 	camisas = camisas.order_by('etiqueta','contextura')
+	pelos = pelos.order_by('etiqueta')
 	return render_to_response('ladder/create_avatar.html', locals(), RequestContext(request))
 
 def simulate(request, origin_pk, target_pk, sex):
