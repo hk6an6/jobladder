0c1f26c8f1efc4ad7038f4cdba37ca0b42329285
nicolasaragon <nico.diaz.aragon@gmail.com>
2013-01-31 12:38:36 -0500

    changed navigation scheme. fixed link on instructions.html

--- ladder/urls.py
+++ ladder/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 #see https://docs.djangoproject.com/en/dev/topics/http/urls/
 
 urlpatterns = patterns('',
-	url(r'^$', 'ladder.views.home'),
+	url(r'^$', 'ladder.views.instructions', name="home_page"),
+	url(r'^start/$', 'ladder.views.home', name="start"),
 	url(r'^cargo/$','ladder.views.cargo', name="get-cargos"),
 	url(r'^cargo/siguientes/(?P<cargo_pk>\d+)/$','ladder.views.cargo_by_pk', name="get-cargo-by-pk"),
 	url(r'^cargo/(?P<cargo_name_fragment>[\w\s]*)/$','ladder.views.cargo', name="get-cargos-filter-by-cargo-name"),
