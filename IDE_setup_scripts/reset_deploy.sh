164e833348dde815311c0384beae548814af91eb
ndiaz <ndiaz@serempre.com>
2013-05-02 11:29:17 -0500

    reset installation script

---
+++ IDE_setup_scripts/reset_deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#takes two positional arguments: <exit status> and a <tag>. Aborts process if $? isn't zero
+#<exit status>: stands for the last process execution status. Zero means everything worked. This can be queried by using $?
+#<tag>: a tag to identify the process. Just use any text that suits you
+abortOnFailure(){
+	if [ $1 -eq 0 ]; then 
+		echo "$2: completed successfully"; 
+	else 
+		echo "$2: failed";
+		exit $1 
+	fi
+}
+
+sudo rm -rf ~/media
+abortOnFailure $? "sudo rm -rf ~/media"
+sudo mv /opt/bitnami/apps/django/django_projects/jobladder/jobladder/media ~/media
+abortOnFailure $? "sudo mv /opt/bitnami/apps/django/django_projects/jobladder/jobladder/media ~/media"
+sudo rm -rf /opt/bitnami/apps/django/django_projects/jobladder
+abortOnFailure $? "sudo rm -rf /opt/bitnami/apps/django/django_projects/jobladder"
+sudo mv ~/jobladder /opt/bitnami/apps/django/django_projects/jobladder
+abortOnFailure $? "sudo mv ~/jobladder /opt/bitnami/apps/django/django_projects/jobladder"
+sudo mv ~/media /opt/bitnami/apps/django/django_projects/jobladder/jobladder/media
+abortOnFailure $? "sudo mv ~/media /opt/bitnami/apps/django/django_projects/jobladder/jobladder/media"
+sudo cp -f /opt/bitnami/apps/django/django_projects/settings.py.bk.latest /opt/bitnami/apps/django/django_projects/jobladder/jobladder/settings.py
+abortOnFailure $? "sudo cp -f /opt/bitnami/apps/django/django_projects/settings.py.bk.latest /opt/bitnami/apps/django/django_projects/jobladder/jobladder/settings.py"
+sudo chmod -R 755 /opt/bitnami/apps/django/django_projects/jobladder
+abortOnFailure $? "sudo chmod -R 755 /opt/bitnami/apps/django/django_projects/jobladder"
+sudo chown -R daemon:root /opt/bitnami/apps/django/django_projects/jobladder
+abortOnFailure $? "sudo chown -R daemon:root /opt/bitnami/apps/django/django_projects/jobladder"
+sudo python /opt/bitnami/apps/django/django_projects/precursor/manage.py collectstatic
+abortOnFailure $? "sudo python /opt/bitnami/apps/django/django_projects/precursor/manage.py collectstatic"
+sudo sh /opt/bitnami/ctlscript.sh restart apache
+abortOnFailure $? "sudo sh /opt/bitnami/ctlscript.sh restart apache"
