f7898fb7724b5fcb0f94cec1b4fe212d501aef80
javiercf <javiercferrans@gmail.com>
2013-05-15 20:19:24 -0500

    fixed bugs

--- jobladder/settings.py
+++ jobladder/settings.py
@@ -4,23 +4,23 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.join(PROJECT_ROOT,'../jobladder')
-DEBUG = False
+DEBUG = True
 USING_FOREMAN = os.getenv('USE_FOREMAN', False)
 USE_POSGRES_USER = False
 #USE_POSGRES_USER = os.getenv('USE_POSGRES_USER', False)
-USE_AWS_S3_STORAGE = False
+USE_AWS_S3_STORAGE = True
 TEMPLATE_DEBUG = DEBUG
 #set the following to true to inspect database queries
 DEBUG_DATABASE_QUERIES = False
 
 ADMINS = (
-    ('Nicolas Diaz Aragon', 'ndiaz@serempre.com'),
-    ('David Panesso', 'dpanesso@serempre.com'),
-    ('Billy Camargo', 'bcamargo@serempre.com'),
-	('Albina Velasco', 'avelasco@advantika.com.co'),
-	('Luisa Murcia', 'lmurcia@advantika.com.co'),
-
-)
+          ('Nicolas Diaz Aragon', 'ndiaz@serempre.com'),
+          ('David Panesso', 'dpanesso@serempre.com'),
+          ('Billy Camargo', 'bcamargo@serempre.com'),
+          ('Albina Velasco', 'avelasco@advantika.com.co'),
+          ('Luisa Murcia', 'lmurcia@advantika.com.co'),
+          
+          )
 
 MANAGERS = ADMINS
 
@@ -33,8 +33,8 @@ if USE_POSGRES_USER:
 			'PASSWORD': 'adminSerempre',                  # Not used with sqlite3.
 			'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
 			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-		}
-	}
+    }
+}
 else:
 	DATABASES = {
 		'default': {
@@ -44,8 +44,8 @@ else:
 			'PASSWORD': '',                  # Not used with sqlite3.
 			'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
 			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-		}
-	}
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -91,39 +91,39 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR,'static/'),
-)
+                    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+                    # Always use forward slashes, even on Windows.
+                    # Don't forget to use absolute paths, not relative paths.
+                    os.path.join(PROJECT_DIR,'static/'),
+                    )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+                       'django.contrib.staticfiles.finders.FileSystemFinder',
+                       'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+                       #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+                       )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'a_fff4#ro^^+d9nwo9p*hp28mlc786xyv0_78&amp;g9x=s!%j-2-%'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                    #     'django.template.loaders.eggs.Loader',
+                    )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+                      'django.middleware.common.CommonMiddleware',
+                      'django.contrib.sessions.middleware.SessionMiddleware',
+                      'django.middleware.csrf.CsrfViewMiddleware',
+                      'django.contrib.auth.middleware.AuthenticationMiddleware',
+                      'django.contrib.messages.middleware.MessageMiddleware',
+                      # Uncomment the next line for simple clickjacking protection:
+                      # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+                      )
 
 ROOT_URLCONF = 'jobladder.urls'
 
@@ -131,26 +131,26 @@ ROOT_URLCONF = 'jobladder.urls'
 WSGI_APPLICATION = 'jobladder.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, 'templates'),
-)
+                 # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+                 # Always use forward slashes, even on Windows.
+                 # Don't forget to use absolute paths, not relative paths.
+                 os.path.join(PROJECT_DIR, 'templates'),
+                 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    'gunicorn',
-    'ladder',
-)
+                  'django.contrib.auth',
+                  'django.contrib.contenttypes',
+                  'django.contrib.sessions',
+                  'django.contrib.sites',
+                  'django.contrib.messages',
+                  'django.contrib.staticfiles',
+                  # Uncomment the next line to enable the admin:
+                  'django.contrib.admin',
+                  # Uncomment the next line to enable admin documentation:
+                  'django.contrib.admindocs',
+                  'gunicorn',
+                  'ladder',
+                  )
 
 if USE_AWS_S3_STORAGE:
 	INSTALLED_APPS += ( 'storages', )
@@ -175,14 +175,14 @@ if DEBUG_DATABASE_QUERIES:
 		'filters': {
 			'require_debug_false': {
 				'()': 'django.utils.log.RequireDebugFalse'
-			}
+        }
 		},
 		'handlers': {
 			'mail_admins': {
 				'level': 'ERROR',
 				'filters': ['require_debug_false'],
 				'class': 'django.utils.log.AdminEmailHandler'
-			}, 
+			},
 			'console':{
 				'level':'DEBUG',
 				'class':'logging.StreamHandler',
@@ -205,8 +205,8 @@ if DEBUG_DATABASE_QUERIES:
 				'level': 'DEBUG',
 				'propagate': True,
 			},
-		}
-	}
+    }
+}
 else:
 	LOGGING = {
 		'version': 1,
@@ -222,14 +222,14 @@ else:
 		'filters': {
 			'require_debug_false': {
 				'()': 'django.utils.log.RequireDebugFalse'
-			}
+        }
 		},
 		'handlers': {
 			'mail_admins': {
 				'level': 'ERROR',
 				'filters': ['require_debug_false'],
 				'class': 'django.utils.log.AdminEmailHandler'
-			}, 
+			},
 			'console':{
 				'level':'DEBUG',
 				'class':'logging.StreamHandler',
@@ -247,8 +247,8 @@ else:
 				'level': 'DEBUG',
 				'propagate': True,
 			},
-		}
-	}
+    }
+}
 
 
 #this adds necessary settings for database to work properly when deployed to heroku
